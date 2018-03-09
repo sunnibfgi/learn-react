318e2ddda2fc3caf5244dc80e6919f7ddee8d5fa
sunnibfgi <sunqijun1523@gmail.com>
2018-03-09 13:18:22 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'js/app.js',
+		libraryTarget: 'umd'
+	},
+	resolve: {
+		extensions: ['.js', 'jsx', '.json'],
+		modules: [
+			path.resolve(__dirname, 'node_modules')
+		]
+	},
+	module: {
+		rules: [
+
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader?cacheDirectory=true',
+					options: {
+						presets: ['es2015', 'react', 'stage-0', 'stage-1']
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+
+ 			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+		})
+	]
+}
