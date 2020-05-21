38929efe057a5b97dab795500432281ee6ffe4f9
Son Ki-Kyun <sunqijun1523@gmail.com>
2020-05-21 16:20:57 +0800

    Update email-autocomplete.js

--- src/email-autocomplete/email-autocomplete.js
+++ src/email-autocomplete/email-autocomplete.js
@@ -1,195 +1,144 @@
-import React from 'react';
+import React from 'react'
 
-class EmailInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.onKeyDownHandler = this.onKeyDownHandler.bind(this);
-    this.onClickItemHandler = this.onClickItemHandler.bind(this);
-  }
 
+const EmailList = React.forwardRef(({emailList, index, visible}, ref) => {
+  return (
+    <div className="content" style={{ display: visible ? '' : 'none'}} ref={ref}>
+      {
+        emailList.sort().map((email, i) => 
+          <Item 
+            key={email} 
+            email={email} 
+            style={{backgroundColor: i === index ? '#eee' : ''}} 
+          />
+        )
+      }
+    </div>
+  )
+})
+
+const Item = ({email, style}) => <p style={{...style}}>{email}</p>
+
+class App extends React.Component {
   state = {
     value: '',
     index: 0,
-    atBeforeValue: '',
-    atAfterValue: '',
-    close: true
-  };
-
-  atBeforeValuePattern = /^[a-zA-Z0-9_@]+$/;
-
-  itemVisibilityStatus(e) {
-    let target = e.target;
-    if (target === this.emailInput) {
-      this.setState({
-        close: !this.atBeforeValuePattern.test(target.value)
-      });
-    } else {
-      this.setState({
-        close: true,
-        index: 0
-      });
-    }
+    visiblePanel: true
   }
-
-  componentWillUpdate() {
-
-  }
-
-  componentDidMount() {
-    document.addEventListener('click', this.itemVisibilityStatus.bind(this));
-  }
-  componentWillUnmount() {
-    document.removeEventListener('click', this.itemVisibilityStatus.bind(this));
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.value !== nextProps.value) {
-      this.setState({
-        value: nextProps.value,
-        index: 0
-      });
-    }
-  }
-
-  onKeyDownHandler(e) {
-    let keyCode = e.keyCode;
-    let filter = this.props.email.filter((el) => {
-      return !el.indexOf(this.state.atAfterValue);
-    });
-    if (!this.state.close && this.atBeforeValuePattern.test(this.state.atBeforeValue)) {
-      switch (keyCode) {
-      case 38:
-        this.setState(prevState => ({
-          index: prevState.index > 0 ? prevState.index - 1 : filter.length - 1
-        }), () => {
-          this.setState({
-            value: `${this.state.atBeforeValue}${filter[this.state.index]}`
-          });
-        });
-        break;
-      case 40:
-        this.setState(prevState => ({
-          index: prevState.index < filter.length - 1 ? prevState.index + 1 : 0
-        }), () => {
-          this.setState({
-            value: `${this.state.atBeforeValue}${filter[this.state.index]}`
-          });
-        });
-        break;
-      case 13:
-        this.setState({
-          close: true,
-          index: 0,
-          value: this.state.atBeforeValue + filter[this.state.index]
-        });
-        break;
-      default:
-        this.setState({
-          index: 0
-        });
-        break;
-      }
-    }
-  }
-  onChangeHandler(e) {
-    let {value} = e.target;
-    let indexOf = value.indexOf('@');
+  filterEmail: null 
+  handleChange = ({target}) => {
+    let value = target.value
+    this.splitValue = ~value.indexOf('@') && (value.indexOf('@') === value.lastIndexOf('@')) ? value.split('@') : null
     this.setState({
       value: value,
-      atBeforeValue: indexOf > 0 ? value.substring(0, indexOf + 1) : '',
-      atAfterValue: indexOf > 0 ? value.substring(indexOf + 1, value.length) : '',
-      close: !(indexOf >= 0)
-    });
+      index: 0,
+      visiblePanel: true
+    })
+  }
+  componentDidMount() {
+    console.log(this.inputRef)
+    this.inputRef.focus()
+    document.addEventListener('click', this.listener = function(e) {
+      if(e.target === this.inputRef) return
+      if(this.containerRef && !this.containerRef.contains(e.target) ) {
+        this.setState({
+          ...this.state,
+          visiblePanel: false
+        })
+      }
+    }.bind(this))
   }
 
-  onClickItemHandler(e) {
-    this.props.getItemText(e.target.innerText);
+  componentWillUnmount() {
+    document.removeEventListener('click', this.listener)
+  }
+
+  getFullEmailValue = (idx, callback) => {
+    let {value} = this.state
+    console.log(value)
     this.setState({
-      close: true,
-      index: 0
-    });
+      value: this.filterEmail[idx] || value
+    }, callback)
+  }
+  
+  handleKeyDown = ({keyCode, nativeEvent}) => {
+    nativeEvent.stopPropagation && nativeEvent.stopPropagation()
+    switch (keyCode) {
+    case 13:
+      this.getFullEmailValue(this.state.index, () => {
+        this.inputRef.blur()
+      })
+      this.splitValue = null
+      return
+    case 38:
+      this.setState(state => ({
+        ...this.state,
+        index: this.splitValue ? (state.index > 0 ? state.index - 1 : this.filterEmail.length - 1) : 0
+      }), () => {
+        this.getFullEmailValue(this.state.index)
+      })
+      return
+    case 40:
+      this.setState(state => ({
+        ...this.state,
+        index: this.splitValue ? (state.index === this.filterEmail.length - 1 ? 0 : state.index + 1) : 0
+      }), () => {
+        this.getFullEmailValue(this.state.index)
+      })
+      return
+    
+    default:
+      return
+    }
+
+  }
+  getRefs = ref => el => {
+    this[ref] = el
   }
 
   render() {
+    let {email} = this.props
+    let {value, index, visiblePanel} = this.state
+
+    this.filterEmail = email.filter(email => {
+      return this.splitValue && ~email.indexOf(this.splitValue[1])
+    }).map(email => {
+      return this.splitValue[0] + email
+    })
+
     return (
-      <div className="email-ct">
-        <p>
-          <input
-            type="text"
-            ref={(input) => (this.emailInput = input) }
-            value={this.state.value}
-            onKeyDown={this.onKeyDownHandler}
-            onChange = {this.onChangeHandler}
-            placeholder="enter email address" />
-        </p>
-        {
-          this.atBeforeValuePattern.test(this.state.atBeforeValue)
-            ? <div className="email-item" style={{display: this.state.close ? 'none' : ''}}>
-              {
-                !this.state.atAfterValue
-                  ? this.props.email
-                    .sort()
-                    .map((el, idx) => {
-                      return <Items
-                        key={idx}
-                        keys = {idx}
-                        index={this.state.index}
-                        el={el}
-                        atBeforeValue = {this.state.atBeforeValue}
-                        onClickItemHandler={this.onClickItemHandler} />;
-                    })
-                  : this.props.email
-                    .filter((el) => {
-                      return !el.indexOf(this.state.atAfterValue);
-                    })
-                    .sort()
-                    .map((el, idx) => {
-                      return <Items
-                        key={idx}
-                        keys = {idx}
-                        index={this.state.index}
-                        el={el}
-                        atBeforeValue = {this.state.atBeforeValue}
-                        onClickItemHandler={this.onClickItemHandler} />;
-                    })
-
-              }
-            </div> : ''
-        }
-
+      <div style={{width:'30%'}} >
+        <input
+          ref={this.getRefs('inputRef')}
+          type="text" 
+          onKeyDown={this.handleKeyDown}
+          onChange={this.handleChange} 
+          value={value}
+          placeholder="enter your email" />
+        
+        <EmailList 
+          visible={visiblePanel}
+          ref={this.getRefs('containerRef')}
+          emailList={this.filterEmail} 
+          index={index} 
+        /> 
       </div>
-    );
+    )
   }
 }
 
-function Items(props) {
-  return (
-    <p style={{backgroundColor: props.keys !== props.index ? '' : '#eee'}}
-      className="item" onClick={props.onClickItemHandler}>{props.atBeforeValue}{props.el}</p>
-  );
-}
+let emailList = [
+  '@chrome.com', 
+  '@firefox.com', 
+  '@firefox.net', 
+  '@opera.net', 
+  '@safari.net',  
+  '@safari.com', 
+  '@opera.com', 
+  '@chrome.net'
+]
 
-class Email extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getItemText = this.getItemText.bind(this);
-  }
-  state= {
-    text: ''
-  };
-  getItemText(text) {
-    this.setState({
-      text
-    });
-  }
-  render() {
-    return (
-      <EmailInput value={this.state.text}
-        email={this.props.email}
-        getItemText={this.getItemText}/>
-    );
-  }
+App.defaultProps = {
+  email: emailList
 }
-
-export default Email;
+export default App
